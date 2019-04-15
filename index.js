b7cb11bdc4dc7675d53b6992d6e39ec2da902eac
Katherine Briceño <kath.gbl@gmail.com>
2019-04-15 14:11:31 +0200

    Add logger function and momed moment package

--- index.js
+++ index.js
@@ -1,6 +1,17 @@
 const express = require("express");
-const app = express();
 const path = require("path");
+const logger = require("./middleware/logger");
+const members = require("./public/Members");
+
+const app = express();
+
+//Make a simple REST API
+
+//Init middleware
+app.use(logger);
+
+//Gets all members
+app.get("/api/members", (req, res) => res.json(members));
 
 //Set Static Folder for Static Server
 app.use(express.static(path.join(__dirname, "public")));
---
+++ public/Members.js
@@ -0,0 +1,28 @@
+const members = [
+  {
+    id: 1,
+    name: "Kath",
+    email: "kath@gmail.com",
+    status: "active"
+  },
+  {
+    id: 2,
+    name: "Bea",
+    email: "bea@gmail.com",
+    status: "inactive"
+  },
+  {
+    id: 3,
+    name: "Tere",
+    email: "tere@gmail.com",
+    status: "active"
+  },
+  {
+    id: 4,
+    name: "Maria",
+    email: "maria@gmail.com",
+    status: "inactive"
+  }
+];
+
+module.exports = members;
