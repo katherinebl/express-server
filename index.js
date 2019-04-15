1ec2942bc95ad4b90d5f5fbff6a7dedfa87b3d56
Katherine Briceño <kath.gbl@gmail.com>
2019-04-15 15:12:56 +0200

    Add 'Members API Routes'

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 const express = require("express");
 const path = require("path");
 const logger = require("./middleware/logger");
-const members = require("./public/Members");
 
 const app = express();
 
@@ -10,22 +9,11 @@ const app = express();
 //Init middleware
 // app.use(logger);
 
-//Gets all members
-app.get("/api/members", (req, res) => res.json(members));
-
-//Get single member
-app.get("/api/members/:id", (req, res) => {
-  const found = members.some(member => member.id === parseInt(req.params.id));
-
-  if (found) {
-    res.json(members.filter(member => member.id === parseInt(req.params.id)));
-  } else {
-    res.status(400).json({ msg: `No member with the id of ${req.params.id}` });
-  }
-});
-
 //Set Static Folder for Static Server
 app.use(express.static(path.join(__dirname, "public")));
+
+//Members API Routes
+app.use("/api/members", require("./routes/api/members"));
 
 const PORT = process.env.PORT || 5000;
 
