2e1b9efef4623b50e3d87916cb87b3f2b92f2077
Katherine Briceño <kath.gbl@gmail.com>
2019-04-15 16:08:00 +0200

    Add 'Create member' funtionality and uuid package

--- index.js
+++ index.js
@@ -9,6 +9,9 @@ const app = express();
 //Init middleware
 // app.use(logger);
 
+//Body parser middleware
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 //Set Static Folder for Static Server
 app.use(express.static(path.join(__dirname, "public")));
 
