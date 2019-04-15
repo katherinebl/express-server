295448a7ce87c5a72e8cc553b4a87ca196ea204e
Katherine Briceño <kath.gbl@gmail.com>
2019-04-15 14:52:00 +0200

    Add 'Get single member' functionality

--- index.js
+++ index.js
@@ -8,10 +8,21 @@ const app = express();
 //Make a simple REST API
 
 //Init middleware
-app.use(logger);
+// app.use(logger);
 
 //Gets all members
 app.get("/api/members", (req, res) => res.json(members));
+
+//Get single member
+app.get("/api/members/:id", (req, res) => {
+  const found = members.some(member => member.id === parseInt(req.params.id));
+
+  if (found) {
+    res.json(members.filter(member => member.id === parseInt(req.params.id)));
+  } else {
+    res.status(400).json({ msg: `No member with the id of ${req.params.id}` });
+  }
+});
 
 //Set Static Folder for Static Server
 app.use(express.static(path.join(__dirname, "public")));
